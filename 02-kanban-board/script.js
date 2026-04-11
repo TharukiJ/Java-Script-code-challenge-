12cdbb083e07bcc8bfe55ed506430492e13424f8
TharukiJ <tharuki.fbacc@gmail.com>
2026-04-11 18:16:33 +0530

    Update script.js

--- 02-kanban-board/script.js
+++ 02-kanban-board/script.js
@@ -20,3 +20,20 @@ function drag(ev) {
     }, 0);
 }
 
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+    setTimeout(() => {
+        ev.target.style.opacity = '0.5';
+    }, 0);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    const data = ev.dataTransfer.getData("text");
+    const task = document.getElementById(data);
+    task.style.opacity = '1';
+
