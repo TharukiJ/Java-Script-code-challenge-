bfd88d40191830348aff6f1b8b2126d5a2606793
TharukiJ <tharuki.fbacc@gmail.com>
2026-04-11 18:30:18 +0530

    Update script.js

--- 03-job-tracker/script.js
+++ 03-job-tracker/script.js
@@ -2,3 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const jobForm = document.getElementById('job-form');
     const jobList = document.getElementById('job-list');
 
+    // Using local storage for data persistence
+    let jobs = JSON.parse(localStorage.getItem('jobs')) || [];
+
